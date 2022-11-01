0b3d79b2d2a5b66e7f3cea9cfe6a7df50eb61e2b
Dannystarrz <danny.starr007@gmail.com>
2022-11-01 18:01:30 +0100

    html structure set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <section className='profile-section'>
+
+        <img src='' alt='' id='profile__img'></img>
+
+        <p id='twitter'>Dannystar007</p>
+
+        <p id='slack'>Dannystarr007</p>
+
+      </section>
+
+      <section className='link-section'>
+
+      <a id='btn__zuri' href="https://twitter.com/DannyStar007"> <button>Twitter Link</button> </a>
+
+      <a id='btn__zuri' href="https://training.zuri.team/"> <button>Zuri Team</button> </a>
+
+      <a id='books' href="http://books.zuri.team"> <button>Zuri Books</button> </a>
+
+      <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=<Dannystarr007>, "> <button>Python Books</button> </a>
+
+      <a id='pitch' href="https://background.zuri.team"> <button>Background check for coders</button> </a>
+
+      <a id='book__design' href="https://books.zuri.team/design-rules"> <button>Design Books</button> </a>
+
+      </section>
+
+      <section className='social-media'>
+
+      </section>
+
+      <section className='footer'>
+
+        <img src='../public/images/Zuri.internship_Logo.png' alt='Zuri logo'>
+
+        </img>
+
+        <p>HNG internship 9 frontend task</p>
+
+        <img src='../public/images/I4G.png/' alt='I4g logo'>
+
+        </img>
+
+      </section>
     </div>
   );
 }
