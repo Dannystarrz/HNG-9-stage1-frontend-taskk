1213243c6d48e9eb920fb5e7dba4fac1361b5726
Dannystarrz <danny.starr007@gmail.com>
2022-11-01 21:22:41 +0100

    style added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
 
       <section className='profile-section'>
 
-        <img src='' alt='' id='profile__img'></img>
+        <img src='../public/images/profile__img(1).png/' alt='profilePhoto' id='profile__img'></img>
+
+        <img src='../public/images/_Avatar share button (1).png' alt='share' id='avatar'></img>
 
         <p id='twitter'>Dannystar007</p>
 
@@ -16,33 +18,37 @@ function App() {
 
       <section className='link-section'>
 
-      <a id='btn__zuri' href="https://twitter.com/DannyStar007"> <button>Twitter Link</button> </a>
+      <a id='btn__zuri' href="https://twitter.com/DannyStar007"> <button className='btns'>Twitter Link</button> </a>
 
-      <a id='btn__zuri' href="https://training.zuri.team/"> <button>Zuri Team</button> </a>
+      <a id='btn__zuri' href="https://training.zuri.team/"> <button className='btns'>Zuri Team</button> </a>
 
-      <a id='books' href="http://books.zuri.team"> <button>Zuri Books</button> </a>
+      <a id='books' href="http://books.zuri.team"> <button className='btns'>Zuri Books</button> </a>
 
-      <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=<Dannystarr007>, "> <button>Python Books</button> </a>
+      <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=<Dannystarr007>, "> <button className='btns'>Python Books</button> </a>
 
-      <a id='pitch' href="https://background.zuri.team"> <button>Background check for coders</button> </a>
+      <a id='pitch' href="https://background.zuri.team"> <button className='btns'>Background check for coders</button> </a>
 
-      <a id='book__design' href="https://books.zuri.team/design-rules"> <button>Design Books</button> </a>
+      <a id='book__design' href="https://books.zuri.team/design-rules"> <button className='btns'>Design Books</button> </a>
 
       </section>
 
       <section className='social-media'>
 
+        <img src='../public/images/slack.png/' alt='slack' id='slack-icon'></img>
+
+        <img src='../public/images/social icon.png/' alt='github'></img>
+
       </section>
 
       <section className='footer'>
 
-        <img src='../public/images/Zuri.internship_Logo.png' alt='Zuri logo'>
+        <img src='../public/images/Zuri.internship_Logo.png' alt='Zuri logo' className='sponsors'>
 
         </img>
 
-        <p>HNG internship 9 frontend task</p>
+        <p className='footer-text'>HNG internship 9 frontend task</p>
 
-        <img src='../public/images/I4G.png/' alt='I4g logo'>
+        <img src='../public/images/I4G.png/' alt='I4g logo' className='sponsors'>
 
         </img>
 
